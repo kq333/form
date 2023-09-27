7016a655144e6f007d1942343e8c2a7187ab2d9d
kq333 <afugowski@yandex.com>
2023-09-27 11:38:10 +0200

    format file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        colors: {
-            inputDefault: '#CBB6E5',
-            inputError: '#ED4545',
-            inputFocus: '#761BE4',
-          },
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    colors: {
+      inputDefault: '#CBB6E5',
+      inputError: '#ED4545',
+      inputFocus: '#761BE4',
     },
-    plugins: [],
-  }
+  },
+  plugins: [],
+};
