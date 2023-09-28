60ffb91732d8319b5aca03569ef89ab7176b6b62
kq333 <afugowski@yandex.com>
2023-09-29 01:28:37 +0200

    import personal info component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import './App.css';
 
+import { PersonalInfo } from './components/Personal-info';
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <main>
+        <section>
+          <PersonalInfo />
+        </section>
+      </main>
+    </div>
+  );
 }
 
 export default App;
